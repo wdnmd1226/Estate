d4a395d8d8cc1347945d6a00487f0777e445de60
dyq <2297344673@qq.com>
2022-05-05 10:44:59 +0800

    Changes

---
+++ src/main/java/com/example/estate/controller/PetController.java
@@ -0,0 +1,13 @@
+package com.example.estate.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 22973
+ */
+@RestController
+@RequestMapping("/pet")
+public class PetController {
+
+}
